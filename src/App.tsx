12ea09fee14b8d0172f182c33b6f113e8176194a
Shudugen <44250965+Shudugen@users.noreply.github.com>
2021-06-23 22:59:52 -0300

    Resultado do quarto dia

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <switch>
+        <Switch>
           <Route path = "/" exact component = {Home} />
           <Route path = "/rooms/new" component = {NewRoom} />
           <Route path = "/rooms/:id" component = {Room} />
-        </switch>
+
+          <Route path = "/admin/rooms/:id" component = {AdminRoom}/>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
